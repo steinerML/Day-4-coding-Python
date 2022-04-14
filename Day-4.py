645318f1d270bdbaf662a956a7a934f5b7b66771
steinerML <102678863+steinerML@users.noreply.github.com>
2022-04-14 19:49:09 +0200

    Add files via upload

---
+++ Day-4.py
@@ -0,0 +1,12 @@
+#Checks whether a number is positive, negative or zero!
+
+number = float(input("Enter your number (-∞ to +∞) : "))
+
+if number > 0 :
+    print("The number is positive")
+elif number < 0 :
+    print("The number is negative")
+else :
+    print("The number is 0!")
+#elif number == 0 :
+    #print("The number is 0!")
